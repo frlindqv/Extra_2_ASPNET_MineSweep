499da4386e1f7b11ff7aa21b3a0eaac985f3149c
frlindqv <frlindqv@kth.se>
2016-02-17 12:10:16 +0100

    New version

---
+++ MineSweep/MineSweep/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MineSweep.Global" Language="C#" %>
---
+++ MineSweep/MineSweep/Scripts/main.js
@@ -0,0 +1,86 @@
+﻿//Global instance 
+var mineSweep = new MineSweep();
+
+
+$(document).ready(function ()
+{
+    $('.carousel').carousel({
+        interval: 7500
+    });
+
+    mineSweep.Player().SetName("Fredrik"); 
+    mineSweep.Player().IncreaseClicks();
+
+    //mineSweep.SetPlayer(new Player("Fredrik"));
+
+    //mineSweep.InitGame();
+    $("h1").click(function () {
+        console.log(mineSweep.Player().Clicks());
+    });
+
+});
+
+
+// Class representing a game of minesweep.
+function MineSweep(){
+
+   //Player attached to the game.
+    var player = new Player();
+
+    // private field holding the current level; 
+    var level;
+
+    // Method responsible for initalizing a new game session.
+    this.InitGame = function () {};
+
+    // Method responsible for running a  running the game.
+    this.RunGame = function () { };
+
+    // Method resposible for ending a game session.
+    this.EndGame = function () { };
+
+    // Get current player
+    this.Player = function () {
+        return player;
+    }
+
+    // Set current player. 
+    this.SetPlayer = function (newPlayer) {
+        player = newPlayer;
+    }
+
+    //this.CreateLevel(length, width)
+    //{
+    //    level = new Array(length);
+
+      
+
+    //}
+
+
+
+}
+
+// Class representing a player. 
+function Player() {
+
+    var name;
+    var time;
+    var clicks = 0;
+
+    this.Name = function (){ return name; };
+
+    this.SetName = function (val) { name = val; };
+
+    this.Clicks = function () { return clicks; };
+
+    this.IncreaseClicks = function () { clicks++; };
+
+}
+
+//Class representing a level
+function Level()
+{
+
+
+}
